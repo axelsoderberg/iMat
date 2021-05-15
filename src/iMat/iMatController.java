84627b34a9c32f399111aba9d0b1727b7070f9db
Axel Söderberg <karlaxel01@hotmail.com>
2021-05-15 14:57:44 +0200

    Städat upp imports och andra varningar

--- src/iMat/iMatController.java
+++ src/iMat/iMatController.java
@@ -4,24 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import se.chalmers.cse.dat216.project.CartEvent;
-import se.chalmers.cse.dat216.project.CreditCard;
-import se.chalmers.cse.dat216.project.Product;
-import se.chalmers.cse.dat216.project.ShoppingCart;
-import se.chalmers.cse.dat216.project.ShoppingCartListener;
 
 
 /**
@@ -34,17 +22,17 @@ public class iMatController implements Initializable/*, ShoppingCartListener*/ {
     @FXML private AnchorPane iMatPane;
 
     // Other variables
-    private final Model model = Model.getInstance();
-    private static List<AnchorPane> panes = new ArrayList<>(); // Här har vi alla fxml-sidor
+    // private final Model model = Model.getInstance();
+    private static final List<AnchorPane> panes = new ArrayList<>(); // Här har vi alla fxml-sidor
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            panes.add(FXMLLoader.load(getClass().getResource("Start.fxml")));
-            panes.add(FXMLLoader.load(getClass().getResource("store.fxml")));
-            panes.add(FXMLLoader.load(getClass().getResource("help.fxml")));
-            panes.add(FXMLLoader.load(getClass().getResource("mypage.fxml")));
-            panes.add(FXMLLoader.load(getClass().getResource("header.fxml")));
+            panes.add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Start.fxml"))));
+            panes.add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("store.fxml"))));
+            panes.add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("help.fxml"))));
+            panes.add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mypage.fxml"))));
+            panes.add(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("header.fxml"))));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -63,19 +51,14 @@ public class iMatController implements Initializable/*, ShoppingCartListener*/ {
 
     // Inte jättesnygg lösning, men använd denna för att få fram den önskade sidan
     private static int getIndexOfPage(String fileName) {
-        switch (fileName) {
-            case "Start":
-                return 0;
-            case "store":
-                return 1;
-            case "help":
-                return 2;
-            case "mypage":
-                return 3;
-            case "header":
-                return 4;
-        }
-        return -1;
+        return switch (fileName) {
+            case "Start" -> 0;
+            case "store" -> 1;
+            case "help" -> 2;
+            case "mypage" -> 3;
+            case "header" -> 4;
+            default -> -1;
+        };
     }
 
 }
