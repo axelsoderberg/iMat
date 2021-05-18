e899a25e6136340df86e5c851fae1c7ed4f4240c
Axel Söderberg <karlaxel01@hotmail.com>
2021-05-18 13:30:02 +0200

    Städat upp imports och andra varningar

--- src/iMat/StartController.java
+++ src/iMat/StartController.java
@@ -1,22 +1,12 @@
 package iMat;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 
-public class StartController implements Initializable/*, ShoppingCartListener*/ {
+public class StartController /*, ShoppingCartListener*/ {
 
     //FXML items
     @FXML private AnchorPane startPane;
@@ -25,11 +15,6 @@ public class StartController implements Initializable/*, ShoppingCartListener*/ 
 
     // Other variables
     iMatController controller = new iMatController();
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
-    }
 
     @FXML
     private void openHelpView(ActionEvent event) {
--- src/iMat/headerController.java
+++ src/iMat/headerController.java
@@ -1,4 +1,28 @@
 package iMat;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+
 public class headerController {
+    //FXML items
+    @FXML private AnchorPane headerPane;
+    @FXML private Button headerHelpButton;
+    @FXML private Button headerStoreButton;
+
+    // Other variables
+    iMatController controller = new iMatController();
+
+    @FXML
+    private void openHelpView(ActionEvent event) {
+        iMatController.setPage("help");
+    }
+
+    @FXML
+    private void openStoreView(ActionEvent event) {
+        iMatController.setPage("store");
+    }
+
 }
